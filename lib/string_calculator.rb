56ed5386c3ed14ab326235ac89ba9b64796a890f
karthickraji <karthickcool17@gmail.com>
2025-01-02 22:42:33 +0530

    4th commit - handle any amount of numbers and handle new lines between numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,10 +1,11 @@
 class StringCalculator
   def add(string_numbers)
-    return string_numbers.to_i if string_numbers.empty?
     if not string_numbers.empty?
       return string_numbers.to_i if string_numbers.length == 1
-      num_arr = string_numbers.split(", ") if string_numbers.length > 1 and string_numbers.include? "," 
-      num_arr.map(&:to_i).sum
+      num_arr = string_numbers.split(",") if string_numbers.length > 1 and string_numbers.include? "," 
+      num_arr.map(&:split).flatten.map(&:to_i).sum
+    else
+      string_numbers.to_i
     end
   end
 end
